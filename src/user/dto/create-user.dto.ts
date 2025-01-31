9aa4849de70d63b2f56a1439d3224181ea3e6dd1
saloum45 <saloumfall45@gmail.com>
2025-01-31 13:44:57 +0000

    dto and entities generated

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,9 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+export class CreateUserDto {
+    @IsEmail()
+    email:string;
+
+    @IsNotEmpty()
+    @MinLength(6)
+    password:string;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id_user: number;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+}
